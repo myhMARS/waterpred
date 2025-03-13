c949ded1cd3a681018bf0c65ef88dc2ae97bda67
mengyonghao <1533512157@qq.com>
2025-03-13 15:55:07 +0800

    原数据集获取

--- model/data_loader.py
+++ model/data_loader.py
@@ -16,14 +16,14 @@ class WaterLevelDataset(Dataset):
         self.file_path = file_path
         self.data = []
 
-        df = pd.read_csv(self.file_path)
+        self.df = pd.read_csv(self.file_path)
         # feature_cols = ['precipitation', 'water_level'] + \
         #                [c for c in df.columns if c.startswith('season_')] + \
         #                ['hour_sin', 'hour_cos', 'month_sin', 'month_cos'] + \
         #                [c for c in df.columns if 'precip_avg' in c or 'level_std' in c]
 
-        X = scaler[0].fit_transform(df.drop(columns=['waterlevels']))
-        y = scaler[1].fit_transform(df.waterlevels_smooth.values.reshape(-1, 1))
+        X = scaler[0].fit_transform(self.df.drop(columns=['waterlevels']))
+        y = scaler[1].fit_transform(self.df.waterlevels_smooth.values.reshape(-1, 1))
         # xgb_model = XGBRegressor()
         # xgb_model.fit(X[:, :-1], y)
         # xgb_pred = xgb_model.predict(X[:, :-1])
@@ -32,13 +32,14 @@ class WaterLevelDataset(Dataset):
         # X[:, -1] = xgb_pred
         # X_data, y_data = [], []
         sequences = []
-        for i in range(len(X) - seq_length - pred_length + 1):
+        for i in range(len(X) - seq_length - pred_length):
             seq_x = (X[i: i + seq_length, :])
             seq_y = (y[i + seq_length: i + seq_length + pred_length, -1])
             sequences.append((seq_x, seq_y))
 
         # features = get_xgboost_feature(*train_data)
-        data_train, data_test = train_test_split(sequences, train_size=split_ratio)
+        train_size = int(len(sequences) * split_ratio)  # 70% 训练
+        data_train, data_test = sequences[:train_size], sequences[train_size:]
 
         self.data = data_train if train else data_test
 
@@ -50,3 +51,6 @@ class WaterLevelDataset(Dataset):
     def __getitem__(self, idx):
         seq_x, seq_y = self.data[idx]
         return torch.tensor(seq_x, dtype=torch.float32), torch.tensor(seq_y, dtype=torch.float32)
+
+    def get_source(self):
+        return self.df.drop(columns=['waterlevels']), self.df.waterlevels_smooth
