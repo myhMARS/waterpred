acb756ca232bffb9c453f16502e76cc675bf49ee
mengyonghao <1533512157@qq.com>
2025-03-27 22:49:56 +0800

    Demo演示页更新

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -45,11 +45,6 @@
                   class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{
                     item.name }}</a>
               </div>
-              <div class="py-6">
-                <a href="#"
-                  class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log
-                  in</a>
-              </div>
             </div>
           </div>
         </DialogPanel>
