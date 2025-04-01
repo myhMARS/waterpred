c81a07d0c426e9490de9ea1c00d9dc3c196dad82
mengyonghao <1533512157@qq.com>
2025-04-01 22:49:15 +0800

    后台管理面板更新

--- backend/lstm/admin.py
+++ backend/lstm/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.db import transaction
+from .models import LSTMModels
+
 
 # Register your models here.
+class LSTMModelAdmin(admin.ModelAdmin):
+    list_display = ('date', 'name', 'rmse')
+
+    # actions = ['delete_selected']
+
+    def delete_selected(self, request, queryset):
+        print('delete run')
+
+
+admin.site.register(LSTMModels, LSTMModelAdmin)
