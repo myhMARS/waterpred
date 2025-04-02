bc93f081bbe056b700001cb92c55a6c72e9255c4
mengyonghao <1533512157@qq.com>
2025-04-02 18:34:52 +0800

    代码格式化

--- frontend/src/views/TeamView.vue
+++ frontend/src/views/TeamView.vue
@@ -1,14 +1,16 @@
 <template>
-  <Header />
+  <Header/>
   <div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:mx-0">
         <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Our team</h2>
-        <p class="mt-6 text-lg/8 text-gray-600">我们是一群充满活力的人，对我们的学习充满热情，并致力于为老师提供最佳结果。</p>
+        <p class="mt-6 text-lg/8 text-gray-600">
+          我们是一群充满活力的人，对我们的学习充满热情，并致力于为老师提供最佳结果。</p>
       </div>
-      <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <ul role="list"
+          class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <li v-for="person in people" :key="person.name">
-          <img class="aspect-3/2 w-full rounded-2xl object-cover" :src="person.imageUrl" alt="" />
+          <img class="aspect-3/2 w-full rounded-2xl object-cover" :src="person.imageUrl" alt=""/>
           <h3 class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">{{ person.name }}</h3>
           <p class="text-base/7 text-gray-600">{{ person.role }}</p>
           <ul role="list" class="mt-6 flex gap-x-6">
@@ -17,7 +19,7 @@
                 <span class="sr-only">X</span>
                 <svg class="size-5" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                   <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.68-.22.68-.48
+                        d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.68-.22.68-.48
                        0-.24-.01-.87-.01-1.7-2.79.61-3.38-1.35-3.38-1.35-.46-1.16-1.11-1.47-1.11-1.47-.91-.63.07-.62.07-.62
                        1 .07 1.53 1.03 1.53 1.03.89 1.52 2.33 1.08 2.9.83.09-.64.35-1.08.63-1.33-2.23-.25-4.57-1.11-4.57-4.93
                        0-1.09.39-1.99 1.03-2.69-.1-.25-.45-1.27.1-2.65 0 0 .84-.27 2.75 1.02A9.64 9.64 0 0 1 12 6.84a9.65 9.65
@@ -55,7 +57,7 @@ const people = [
     name: 'Eternity',
     role: '后端/模型开发',
     imageUrl:
-      'src/assets/img/Eternity.png',
+        'src/assets/img/Eternity.png',
     githubUrl: 'https://github.com/myhMARS',
     linkedinUrl: '#',
   },
@@ -63,7 +65,7 @@ const people = [
     name: 'Aurora',
     role: '前端页面开发',
     imageUrl:
-      'src/assets/img/Aurora.png',
+        'src/assets/img/Aurora.png',
     xUrl: '#',
     linkedinUrl: '#',
   },
