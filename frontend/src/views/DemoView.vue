a411608c11a0fd8dff24ee470c2b8400366bd24d
mengyonghao <1533512157@qq.com>
2025-04-03 12:36:07 +0800

    自动更新视图

--- frontend/src/views/DemoView.vue
+++ frontend/src/views/DemoView.vue
@@ -40,60 +40,73 @@ export default {
     apexchart: VueApexCharts,
     Chart,
   },
+
   data() {
     return {
       charts: [],
     };
   },
-  mounted() {
-    axios.get('/api/waterinfo/')
-        .then(response => {
-          const data = response.data;
-          const times = [];
-          const waterlevels = [];       // 桥东村水位
-          const waterlevels63000100 = []; // 里畈水库当前水位
-          const waterlevels63000120 = []; // 里畈东坑溪水位
 
-          data.forEach(item => {
-            times.push(new Date(item.times).toLocaleString('zh-CN'));
-            waterlevels.push(item.waterlevels);
-            waterlevels63000120.push(item.waterlevels63000120);
-            waterlevels63000100.push(item.waterlevels63000100);
+  methods: {
+    async fetchData() {
+      axios.get('/api/waterinfo/')
+          .then(response => {
+            this.charts = []
+            const data = response.data;
+            const times = [];
+            const waterlevels = [];       // 桥东村水位
+            const waterlevels63000100 = []; // 里畈水库当前水位
+            const waterlevels63000120 = []; // 里畈东坑溪水位
+
+            data.forEach(item => {
+              times.push(new Date(item.times).toLocaleString('zh-CN'));
+              waterlevels.push(item.waterlevels);
+              waterlevels63000120.push(item.waterlevels63000120);
+              waterlevels63000100.push(item.waterlevels63000100);
+            });
+            console.log(this.charts)
+            // 更新图表数据
+            const chart1 = new CustomChart(
+                "里畈东坑溪水位",
+                times,
+                'mm',
+                [{type: current_waterlevel, data: waterlevels63000120}]
+            );
+            // const chart2 = new chart("里畈水库水位",times,'mm',["水位"],[waterlevels], ["#4CAF50"])
+            const chart2 = new CustomChart(
+                "里畈水库水位",
+                times,
+                'mm',
+                [
+                  {type: current_waterlevel, data: waterlevels63000100},
+                  {type: flood_limit_water_level, data: new Array(times.length).fill(234.73)}
+                ]
+            );
+            const chart3 = new CustomChart(
+                "桥东村水位",
+                times,
+                'mm',
+                [
+                  {type: current_waterlevel, data: waterlevels},
+                  {type: guaranteed_water_level, data: new Array(times.length).fill(85.66)},
+                  {type: warning_water_level, data: new Array(times.length).fill(84.66)}
+                ]
+            )
+            this.charts.push(chart1, chart2, chart3)
+          })
+          .catch(error => {
+            console.error('请求失败:', error);
           });
-          console.log(this.charts)
-          // 更新图表数据
-          const chart1 = new CustomChart(
-              "里畈东坑溪水位",
-              times,
-              'mm',
-              [{type: current_waterlevel, data: waterlevels63000120}]
-          );
-          // const chart2 = new chart("里畈水库水位",times,'mm',["水位"],[waterlevels], ["#4CAF50"])
-          const chart2 = new CustomChart(
-              "里畈水库水位",
-              times,
-              'mm',
-              [
-                {type: current_waterlevel, data: waterlevels63000100},
-                {type: flood_limit_water_level,data: new Array(times.length).fill(234.73)}
-              ]
-          );
-          const chart3 = new CustomChart(
-              "桥东村水位",
-              times,
-              'mm',
-              [
-                {type:current_waterlevel, data: waterlevels},
-                {type: guaranteed_water_level, data: new Array(times.length).fill(85.66)},
-                {type:warning_water_level,data: new Array(times.length).fill(84.66)}
-              ]
-          )
-          this.charts.push(chart1,chart2,chart3)
-        })
-        .catch(error => {
-          console.error('请求失败:', error);
-        });
+    }
   },
+
+  mounted() {
+    this.fetchData();
+    this.interval = setInterval(this.fetchData, 5000);
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  }
 }
 </script>
 
