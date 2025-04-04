7307c897c2a1628f9a9eae1ddb143afba283d154
mengyonghao <1533512157@qq.com>
2025-04-04 17:27:40 +0800

    注册登录页面实现

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,37 +1,39 @@
 <template>
-  <Header />
+  <Header/>
   <section class="grid text-center h-screen items-center p-8">
     <div>
       <h3 class="text-3xl font-semibold text-blue-gray-900 mb-2">登 录</h3>
       <p class="text-gray-600 mb-16 text-[18px]">
         输入你的用户名和密码以登录
       </p>
-      <form @submit.prevent="handleSubmit" class="mx-auto max-w-[24rem] text-left">
+      <form @submit.prevent="login" class="mx-auto max-w-[24rem] text-left">
         <div class="mb-6">
           <label for="username" class="block font-medium text-gray-900 mb-2">用户名</label>
           <input
-            id="username"
-            type="text"
-            v-model="username"
-            placeholder="Enter your username"
-            class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
-            required
+              id="username"
+              type="text"
+              v-model="username"
+              placeholder="Enter your username"
+              class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
+              autocomplete="username"
+              required
           />
         </div>
         <div class="mb-6">
           <label for="password" class="block font-medium text-gray-900 mb-2">密码</label>
           <input
-            id="password"
-            type="password"
-            v-model="password"
-            placeholder="********"
-            class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
-            required
+              id="password"
+              type="password"
+              v-model="password"
+              placeholder="********"
+              class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
+              autocomplete="current-password"
+              required
           />
         </div>
         <button
-          type="submit"
-          class="w-full py-3.5 px-7 rounded-lg bg-gray-900 text-white shadow-md hover:shadow-lg mt-6"
+            type="submit"
+            class="w-full py-3.5 px-7 rounded-lg bg-gray-900 text-white shadow-md hover:shadow-lg mt-6"
         >
           登录
         </button>
@@ -40,7 +42,7 @@
         </div>
         <p class="text-gray-700 text-center mt-4">
           没有账号?
-          <a href="#" class="font-medium text-gray-900">注册</a>
+          <a href="/register" class="font-medium text-gray-900">注册</a>
         </p>
       </form>
     </div>
@@ -49,8 +51,11 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import showToast from "@/utils/message.js";
+import axios from "axios";
 
 export default {
+  name: 'login',
   components: {Header},
   data() {
     return {
@@ -59,10 +64,38 @@ export default {
     };
   },
   methods: {
-    handleSubmit() {
-      console.log("Username:", this.username);
-      console.log("Password:", this.password);
-      alert("Login submitted");
+    login() {
+      const username = this.username
+      const password = this.password
+      const fromData = {
+        'username': username,
+        'password': password
+      }
+      axios
+          .post("auth/jwt/create/", fromData)
+          .then(response => {
+            const token = response.data.access
+            const refreshToken = response.data.refresh
+            const username = this.username
+
+            localStorage.setItem("token", token)
+            localStorage.setItem("refreshToken", refreshToken)
+            localStorage.setItem("username", username)
+            localStorage.setItem("expiredTime", Date.now() + 15 * 60 * 1000)
+
+            showToast("success", "登录成功", () => {
+              this.$router.push({
+                name: 'home'
+              })
+            })
+          })
+          .catch(error => {
+            const errorData = error.response.data
+            const errorMessage = Object.values(errorData).flat()
+            for (let i = 0; i < errorMessage.length; i++) {
+              showToast('error', errorMessage[i])
+            }
+          })
     }
   }
 };
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -4,7 +4,7 @@
     <div>
       <h3 class="text-3xl font-semibold text-blue-gray-900 mb-2">注 册</h3>
 
-      <form @submit.prevent="handleSubmit" class="mx-auto max-w-[24rem] text-left">
+      <form @submit.prevent="register" class="mx-auto max-w-[24rem] text-left">
         <div class="mb-6">
           <label for="username" class="block font-medium text-gray-900 mb-2">用户名</label>
           <input
@@ -13,6 +13,7 @@
               v-model="username"
               placeholder="Enter your username"
               class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
+              autocomplete="username"
               required
           />
         </div>
@@ -24,17 +25,19 @@
               v-model="password"
               placeholder="********"
               class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
+              autocomplete="new-password"
               required
           />
         </div>
         <div class="mb-6">
           <label for="password" class="block font-medium text-gray-900 mb-2">再次输入密码</label>
           <input
-              id="password"
+              id="re_password"
               type="password"
-              v-model="password"
+              v-model="re_password"
               placeholder="********"
               class="w-full h-11 px-3 py-3 rounded-md border border-blue-gray-200 focus:border-gray-900"
+              autocomplete="new-password"
               required
           />
         </div>
@@ -51,20 +54,46 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import showToast from "@/utils/message.js";
+import axios from "axios";
+
 
 export default {
   components: {Header},
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      re_password: ''
     };
   },
   methods: {
-    handleSubmit() {
-      console.log("Username:", this.username);
-      console.log("Password:", this.password);
-      alert("Login submitted");
+    register() {
+      const username = this.username
+      const password = this.password
+      const re_password = this.re_password
+      if (password !== re_password) {
+        showToast('error', '两次输入密码不一致')
+        return
+      }
+      const formData = {
+        'username': username,
+        'password': password
+      }
+      axios
+          .post('/auth/users/', formData)
+          .then(response => {
+            showToast('success', '注册成功',()=>{
+              this.$router.push({name:'login'})
+            })
+          })
+          .catch(error => {
+            const errorData = error.response.data
+            const errorMessage = Object.values(errorData).flat()
+            for (let i=0; i<errorMessage.length;i++) {
+              showToast('error',errorMessage[i])
+            }
+          })
     }
   }
 };
