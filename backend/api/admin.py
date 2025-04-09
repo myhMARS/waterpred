9e819f811213e54420e7640a83b3c79ea9ec74aa
mengyonghao <1533512157@qq.com>
2025-04-09 19:34:28 +0800

    新增水文数据管理面板

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import WaterInfo, WaterPred
+
+
 # Register your models here.
+@admin.register(WaterInfo)
+class WaterInfoAdmin(admin.ModelAdmin):
+    list_display = (
+        'times', 'temperature', 'humidity', 'winddirection', 'windpower',
+        'rains', 'waterlevels63000120', 'rains63000100',
+        'waterlevels63000100', 'waterlevels'
+    )
+    search_fields = ('times',)
+
+
+@admin.register(WaterPred)
+class WaterPredAdmin(admin.ModelAdmin):
+    list_display = (
+        "times", "waterlevel1", "waterlevel2", "waterlevel3",
+        "waterlevel4", "waterlevel5", "waterlevel6"
+    )
+    search_fields = ('times',)
