b0f8df71938fe43e4293dfeb81dc08e2ba18d01e
mengyonghao <1533512157@qq.com>
2025-04-09 19:35:37 +0800

    预测序列化器更新

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import WaterInfo
+from .models import WaterInfo, WaterPred
 
 
 class PredictInputSerializer(serializers.Serializer):
@@ -45,3 +45,9 @@ class WaterInfoTimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = WaterInfo
         fields = ["times"]
+
+
+class WaterPredDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WaterPred
+        fields = ["waterlevel1", "waterlevel2", "waterlevel3", "waterlevel4", "waterlevel5", "waterlevel6"]
