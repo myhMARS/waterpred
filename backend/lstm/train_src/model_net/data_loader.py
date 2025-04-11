7a4ea3137edc93a192b8fd700ee5e770d15acd48
mengyonghao <1533512157@qq.com>
2025-04-11 23:42:13 +0800

    数据集加载方式更新

--- backend/lstm/train_src/model_net/data_loader.py
+++ backend/lstm/train_src/model_net/data_loader.py
@@ -4,17 +4,25 @@ from torch.utils.data import Dataset
 
 
 class WaterLevelDataset(Dataset):
-    def __init__(self, df, train, seq_length, pred_length, scaler, split_ratio=0.8):
+    def __init__(self, X_list, y_list, train, seq_length, pred_length, scaler, split_ratio=0.8):
         self.train = train
-        self.df = df
+        self.X_list = X_list
+        self.y_list = y_list
 
-        X = scaler[0].fit_transform(self.df)
-        y = scaler[1].fit_transform(self.df.waterlevels.values.reshape(-1, 1))
+        datas_X = pd.concat(X_list, axis=0)
+        datas_y = pd.concat(y_list, axis=0)
+        scaler[0].fit(datas_X)
+        scaler[1].fit(datas_y.values.reshape(-1, 1))
+
         sequences = []
-        for i in range(len(X) - seq_length - pred_length):
-            seq_x = (X[i: i + seq_length, :])
-            seq_y = (y[i + seq_length: i + seq_length + pred_length, -1])
-            sequences.append((seq_x, seq_y))
+        for X_data, y_data in zip(X_list, y_list):
+            X = scaler[0].transform(X_data)
+            y = scaler[1].transform(y_data.values.reshape(-1, 1))
+
+            for i in range(len(X) - seq_length - pred_length):
+                seq_x = X[i: i + seq_length, :]
+                seq_y = y[i + seq_length: i + seq_length + pred_length, 0]
+                sequences.append((seq_x, seq_y))
         train_size = int(len(sequences) * split_ratio)  # 70% 训练
         data_train, data_test = sequences[:train_size], sequences[train_size:]
         self.train_data = data_train
@@ -31,4 +39,4 @@ class WaterLevelDataset(Dataset):
         return torch.tensor(seq_x, dtype=torch.float32), torch.tensor(seq_y, dtype=torch.float32)
 
     def get_source(self):
-        return self.df.drop(columns=['waterlevels']), self.df.waterlevels_smooth
+        return self.X_list, self.y_list
