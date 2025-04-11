c0b53c5e4550b1a412289f5c375b375212bd7e09
mengyonghao <1533512157@qq.com>
2025-04-11 23:44:12 +0800

    lstm ORM结构更新 新增站点预测依赖项ORM 通用性扩展

--- backend/lstm/models.py
+++ backend/lstm/models.py
@@ -1,15 +1,47 @@
 from django.db import models
-# from api.models import StationInfo  # feature
+from api.models import StationInfo
+
+
+class PredictDependence(models.Model):
+    station = models.OneToOneField(
+        StationInfo,
+        on_delete=models.CASCADE,
+        related_name='predict_depend',
+        db_column='station_id',
+        to_field='id'
+    )
+    '''JSON字段构成如下
+    {
+        data:
+            AreaWeatherInfo: [fields],
+            Station_id: [fields],
+            ...
+        target:
+            Station_id: [field] 
+    }
+    '''
+    dependence = models.JSONField(verbose_name="训练数据构成")
 
 
 class LSTMModels(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
+    station = models.ForeignKey(
+        StationInfo,
+        on_delete=models.CASCADE,
+        related_name='lstm_model',
+        db_column='station_id',
+        to_field='id',
+        verbose_name="站点id"
+    )
+    name = models.CharField(max_length=100, verbose_name="文件名")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="训练日期")
+    input_size = models.IntegerField()
+    hidden_size = models.IntegerField()
+    output_size = models.IntegerField()
     file = models.FileField(upload_to='LSTM_Models/')
     rmse = models.FloatField()
     md5 = models.CharField(max_length=32, unique=True)  # 确保唯一性
-    is_activate = models.BooleanField(default=False)
+    is_activate = models.BooleanField(default=False, verbose_name="启用状态")
 
 
 class ScalerPT(models.Model):
@@ -28,7 +60,7 @@ class ScalerPT(models.Model):
 
 class TrainResult(models.Model):
     id = models.AutoField(primary_key=True)
-    image = models.FileField(upload_to='TrainResult/')
+    image = models.ImageField(upload_to='TrainResult/')
     lstm_model = models.ForeignKey(
         LSTMModels,
         on_delete=models.CASCADE,
@@ -36,27 +68,3 @@ class TrainResult(models.Model):
         db_column='lstm_md5',
         to_field='md5'
     )
-
-
-""" Feature: 预测依赖链
-class PredictDependnece(models.Model):
-    station = models.ForeignKey(
-        StationInfo,
-        on_delete=models.CASCADE,
-        related_name='predict_depend',
-        db_column='station_id',
-        to_field='id',
-        primary_key=True
-    )
-    '''JSON字段构成如下
-    {
-        data:
-            AreaWeatherInfo: [fields],
-            Station_id: [fields],
-            ...
-        target:
-            Station_id: [field] 
-    }
-    '''
-    dependence = models.JSONField(verbose_name="训练数据构成")
-"""
