5a8a3e779a18131258de59e509b241f7923dce3a
mengyonghao <1533512157@qq.com>
2025-04-11 23:45:59 +0800

    lstm ORM更新导致的训练逻辑变更

--- backend/lstm/tasks.py
+++ backend/lstm/tasks.py
@@ -7,10 +7,11 @@ from datetime import datetime
 from celery import shared_task
 from django.core.files import File
 
-from api.models import WaterInfo
-from .models import LSTMModels, ScalerPT
-from .serializers import WaterInfoSerializer
+from .models import LSTMModels, ScalerPT, PredictDependence, TrainResult
+from api.models import StationInfo
+from .serializers import PredictDependenceSerializer
 from .train_src.train import train_lstm_model
+from .utils import DependenceParser
 
 
 def get_file_md5(filepath):
@@ -24,36 +25,47 @@ def get_file_md5(filepath):
 
 @shared_task
 def start_train():
-    time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    input_size = 8
-    hidden_size = 64
-    output_size = 6
-
-    queryset = WaterInfo.objects.order_by("-times")
-    data = WaterInfoSerializer(queryset, many=True).data
-    if len(data) < 6000:
-        logging.info("data not enough")
-        return 0
-    rmse = train_lstm_model(data, input_size, hidden_size, output_size, time_stamp)
-    if rmse:
-        lstm_filename = f'waterlevel_model_{input_size}_{hidden_size}_{output_size}_{time_stamp}.pt'
-        md5 = get_file_md5(f'temp/{lstm_filename}')
-        with open(f'temp/{lstm_filename}', 'rb') as f:
-            lstm_instance = LSTMModels.objects.create(
-                name=lstm_filename,
-                file=File(f, name=lstm_filename),
-                rmse=rmse,
-                md5=md5
-            )
-        with open(f'temp/scaler_{time_stamp}.pkl', 'rb') as f:
-            ScalerPT.objects.create(
-                name=f'scaler_{time_stamp}',
-                file=File(f, name=f'scaler_{time_stamp}.pkl'),
-                lstm_model=lstm_instance
-            )
-        temp_dir = "temp/"
-        if os.path.exists(temp_dir):
-            shutil.rmtree(temp_dir)
-            os.makedirs(temp_dir)
-        return 1
+    queryset = PredictDependence.objects.all()
+    dependence = PredictDependenceSerializer(queryset, many=True)
+    for dependence in dependence.data:
+        station = dependence['station']
+        dependence_parser = DependenceParser(dependence)
+        if dependence_parser.train_availble:
+            time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            input_size = dependence_parser.input_size
+            hidden_size = 128
+            output_size = 6
+            X_list = dependence_parser.X
+            y_list = dependence_parser.y
+            rmse = train_lstm_model(X_list, y_list, input_size, hidden_size, output_size, time_stamp)
+            if rmse:
+                lstm_filename = f'waterlevel_model_{input_size}_{hidden_size}_{output_size}_{time_stamp}.pt'
+                md5 = get_file_md5(f'temp/{lstm_filename}')
+                with open(f'temp/{lstm_filename}', 'rb') as f:
+                    lstm_instance = LSTMModels.objects.create(
+                        name=lstm_filename,
+                        file=File(f, name=lstm_filename),
+                        input_size=input_size,
+                        hidden_size=hidden_size,
+                        output_size=output_size,
+                        rmse=rmse,
+                        md5=md5,
+                        station=StationInfo.objects.get(id=station),
+                    )
+                with open(f'temp/scaler_{time_stamp}.pkl', 'rb') as f:
+                    ScalerPT.objects.create(
+                        name=f'scaler_{time_stamp}',
+                        file=File(f, name=f'scaler_{time_stamp}.pkl'),
+                        lstm_model=lstm_instance
+                    )
+                with open(f'temp/train_res_{time_stamp}.svg', 'rb') as f:
+                    TrainResult.objects.create(
+                        image=File(f, name=f'train_res_{time_stamp}.svg'),
+                        lstm_model=lstm_instance
+                    )
+                temp_dir = "temp/"
+                if os.path.exists(temp_dir):
+                    shutil.rmtree(temp_dir)
+                    os.makedirs(temp_dir)
+                return 1
     return 0
