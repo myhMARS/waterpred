e14740c7889b72856ff510e006b2e66d9b14e2cb
mengyonghao <1533512157@qq.com>
2025-04-12 21:55:02 +0800

    模型预测逻辑更新

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,51 +1,47 @@
 import numpy as np
 from django.http import Http404
+from datetime import datetime
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import WaterInfo, WaterPred
-from .serializers import PredictInputSerializer, PredictOutputSerializer, WaterInfoDataSerializer, \
-    WaterInfoTimeSerializer, WaterPredDataSerializer
-from .utils import predict
+from .serializers import WaterInfoDataSerializer, WaterInfoTimeSerializer, WaterPredDataSerializer
+from .utils import predict, WaterInfoDependenceParser
+
+from lstm.models import PredictDependence
+from lstm.serializers import PredictDependenceSerializer
 
 
-class Water_Predict(APIView):
+class TaskTest(APIView):
     def get(self, request):
-        raise Http404
-
-    def post(self, request):
-        serializer = PredictInputSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        data = np.array(serializer.validated_data["features"])
-        output = predict(data)
-
-        output_serializer = PredictOutputSerializer({'prediction': output.tolist()})
-        return Response(output_serializer.data, status=status.HTTP_200_OK)
+        from .tasks import update
+        update.delay('http://127.0.0.1:5000/api/weather', 'http://127.0.0.1:5000/api/waterinfo')
+        return Response({'status': 'ok'})
 
 
 class Water_Info(APIView):
     def get(self, request):
-        try:
-            waterinfo = WaterInfo.objects.order_by("-times")[:18][::-1]
-            waterinfo_data = WaterInfoDataSerializer(waterinfo, many=True)
+        station_id = request.query_params.get("station_id")
+        time_length = request.query_params.get("length", default=18)
+        if not station_id:
+            return Response({"detail": "prarms station_id required"}, status=status.HTTP_400_BAD_REQUEST)
+        waterinfo = WaterInfo.objects.filter(station=station_id).order_by('-times')[:time_length][::-1]
+        if not waterinfo:
+            return Response({"detail": "station info notfound"}, status=status.HTTP_400_BAD_REQUEST)
+        waterinfo_data = WaterInfoDataSerializer(waterinfo, many=True)
 
-            waterinfo_time = WaterInfoTimeSerializer(waterinfo, many=True)
-            times = []
-            for _ in waterinfo_time.data:
-                times.extend(_.values())
-            response_data = {
-                "times": times,
-                "data": waterinfo_data.data,
-            }
+        waterinfo_time = WaterInfoTimeSerializer(waterinfo, many=True)
+        times = []
+        for time in waterinfo_time.data:
+            times.extend(time.values())
+        response_data = {
+            "times": times,
+            "data": waterinfo_data.data,
+        }
+        waterpred = WaterPred.objects.filter(times=times[-1], station=station_id)
+        if waterpred.exists():
+            waterpred_data = WaterPredDataSerializer(waterpred, many=True)
+            response_data["pred"] = list(waterpred_data.data[0].values())
 
-            waterpred = WaterPred.objects.order_by("-times").first()
-            waterpred_data = WaterPredDataSerializer(waterpred)
-            if None not in list(waterpred_data.data.values()):
-                response_data["pred"] = list(waterpred_data.data.values())
-
-            return Response(response_data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(response_data, status=status.HTTP_200_OK)
