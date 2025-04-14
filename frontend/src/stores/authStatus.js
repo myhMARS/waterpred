00291bb9ba9979dac2a006db0f8b97d1e279ceda
mengyonghao <1533512157@qq.com>
2025-04-14 23:46:29 +0800

    format

--- frontend/src/stores/authStatus.js
+++ frontend/src/stores/authStatus.js
@@ -1,19 +1,19 @@
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    isLogin: false
-  }),
-  actions: {
-    initializeStore() {
-      this.isLogin = !!localStorage.getItem('token');
-    },
-    setLoginStatus(status,router) {
-      this.isLogin = status
-      if (!status) {
-        localStorage.clear()
-        router.push({ name: 'home' })
-      }
+    state: () => ({
+        isLogin: false,
+    }),
+    actions: {
+        initializeStore() {
+            this.isLogin = !!localStorage.getItem('token');
+        },
+        setLoginStatus(status, router) {
+            this.isLogin = status
+            if (!status) {
+                localStorage.clear()
+                router.push({name: 'home'})
+            }
+        }
     }
-  }
 })
