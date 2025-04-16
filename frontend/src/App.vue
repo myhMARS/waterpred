814802a8db48611c7022e7703416635b3797c1f0
mengyonghao <1533512157@qq.com>
2025-04-16 14:35:21 +0800

    转为vue3组合式写法

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,24 +6,13 @@
   </ThemeProvider>
 </template>
 
-<script>
-import ThemeProvider from './components/layout/ThemeProvider.vue'
-import SidebarProvider from './components/layout/SidebarProvider.vue'
+<script setup>
+import ThemeProvider from '@/components/layout/ThemeProvider.vue'
+import SidebarProvider from '@/components/layout/SidebarProvider.vue'
 import { RouterView } from 'vue-router'
-import { useAuthStore} from "@/stores/authStatus.js";
+import { useAuthStore } from '@/stores/authStatus.js'
 
-
-
-export default {
-  name: 'App',
-  components:{
-    RouterView,
-    ThemeProvider,
-    SidebarProvider
-  },
-  beforeCreate() {
-    const authStore = useAuthStore()
-    authStore.initializeStore()
-  }
-}
+// 在 setup 中使用生命周期函数初始化 store
+const authStore = useAuthStore()
+authStore.initializeStore()
 </script>
