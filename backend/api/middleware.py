c2b912ef1d3273a4afe03b63e8a5a0002e415d3c
mengyonghao <1533512157@qq.com>
2025-04-22 11:14:43 +0800

    LSTM模型归一化器加载模式变更，警告邮件消息队列维护

--- backend/api/middleware.py
+++ backend/api/middleware.py
@@ -1,12 +1,17 @@
+import json
+
 import joblib
 import torch
-
 from django.core.cache import cache
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from lstm.models import LSTMModels, ScalerPT
 from lstm.train_src.model_net.net import Waterlevel_Model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from backend.logging_config import get_task_logger
+
+logger = get_task_logger(__name__)
 
 
 class ModelMiddleware:
@@ -14,7 +19,8 @@ class ModelMiddleware:
         self.get_response = get_response
         self.load_model()
 
-    def load_model(self):
+    @staticmethod
+    def load_model():
         """从数据库加载模型和 scaler，并存入缓存"""
         models_pt = LSTMModels.objects.filter(is_activate=True).all()
         for model_pt in models_pt:
@@ -27,8 +33,7 @@ class ModelMiddleware:
             model.eval()
 
             scaler = joblib.load(scaler_path.file)
-            for _ in scaler:
-                _.feature_names_in_ = None
+            scaler.feature_names_in_ = None
 
             model_info_dict = {
                 "model": model,
@@ -38,6 +43,34 @@ class ModelMiddleware:
             }
 
             cache.set(model_pt.station_id, model_info_dict, timeout=None)
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+
+class WarningMessageTaskMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.init_task()
+
+    def init_task(self):
+        try:
+            schedule, _ = IntervalSchedule.objects.get_or_create(
+                every=3,
+                period=IntervalSchedule.SECONDS,
+            )
+
+            PeriodicTask.objects.update_or_create(
+                name='警告通知管理',
+                defaults={
+                    'interval': schedule,
+                    'task': 'api.tasks.WarngingNoticeManage',
+                    'args': json.dumps([]),
+                }
+            )
+        except Exception as e:
+            logger.error(e, self.__class__.__name__)
 
     def __call__(self, request):
         response = self.get_response(request)
