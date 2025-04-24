ad0b861314b839d1caa5c6dac94bca0794d94a43
mengyonghao <1533512157@qq.com>
2025-04-24 23:27:38 +0800

    异常站点数量统计数据表插入逻辑

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -9,11 +9,20 @@ from django.db.models import Max
 
 from backend.logging_config import get_task_logger
 from lstm.models import LSTMModels
-from .models import WaterInfo, WaterPred, StationInfo, AreaWeatherInfo, WarningNotice
+from .models import WaterInfo, WaterPred, StationInfo, AreaWeatherInfo, WarningNotice, Statistics
 from .serializers import WaterInfoDataSerializer
 from .utils import predict, sendWarning
 
 logger = get_task_logger()
+
+
+def insert_waring_statistics(times: datetime, station_id: str):
+    Statistics.objects.update_or_create(
+        year=times.year,
+        month=times.month,
+        day=times.day,
+        station_id=station_id,
+    )
 
 
 async def fetch_data(keys_dict_list, max_concurrency=50):
@@ -71,7 +80,9 @@ def insert_water_data(data):
         rains=rains,
         waterlevels=waterlevels
     )
-    check_warning(datetime.strptime(data['times'], "%Y-%m-%d %H:%M:%S"), station_id, [waterlevels])
+    queryset = LSTMModels.objects.filter(station_id=station_id, is_activate=True)
+    if queryset.exists():
+        update_predict(station_id)
     logger.debug(f"插入waterinfo {station_id}-{times}")
 
 
@@ -101,31 +112,34 @@ def update_predict(station_id):
     queryset = WaterInfo.objects.filter(station_id=station_id).order_by('-times').all()[:12][::-1]
     waterinfos = WaterInfoDataSerializer(queryset, many=True)
     times = [item['times'] for item in waterinfos.data]
-    try:
-        predict_flag = True
-        times_check = [datetime.fromisoformat(time_str) for time_str in times]
-        for i in range(1, len(times)):
-            time_diff = times_check[i] - times_check[i - 1]
-            if time_diff != timedelta(hours=1):
-                predict_flag = False
-                break
-        if predict_flag and len(queryset) >= 12:
-            times = waterinfos.data[-1]['times']
-            times = datetime.fromisoformat(times)
-            data = [float(item.waterlevels) for item in queryset]
-            output = predict(station_id, data).tolist()[0]
+    current_time = waterinfos.data[-1]['times']
+    current_time = datetime.fromisoformat(current_time)
+    # print(current_time.year, current_time.month, current_time.day, current_time.hour, current_time.minute)
+    # try:
+    predict_flag = True
+    current_waterlevel = [waterinfos.data[-1]['waterlevels']]
+    output = []
+    times_check = [datetime.fromisoformat(time_str) for time_str in times]
+    for i in range(1, len(times)):
+        time_diff = times_check[i] - times_check[i - 1]
+        if time_diff != timedelta(hours=1):
+            predict_flag = False
+            break
+    if predict_flag and len(queryset) >= 12:
+        data = [float(item.waterlevels) for item in queryset]
+        output = predict(station_id, data).tolist()[0]
 
-            fields = {f"waterlevel{i + 1}": level for i, level in enumerate(output)}
-            WaterPred.objects.update_or_create(
-                times=times,
-                station=StationInfo.objects.filter(id=station_id).first(),
-                **fields,
-            )
-            logger.debug(f'插入预测数据{station_id}-{times}')
-            check_warning(timezone.make_naive(times), station_id, output)
-            return 1
-    except Exception as e:
-        logger.error(f"{e},{station_id}-{times}")
+        fields = {f"waterlevel{i + 1}": level for i, level in enumerate(output)}
+        WaterPred.objects.update_or_create(
+            times=current_time,
+            station=StationInfo.objects.filter(id=station_id).first(),
+            **fields,
+        )
+        logger.debug(f'插入预测数据{station_id}-{times}')
+    output = current_waterlevel + output
+    check_warning(timezone.make_naive(current_time), station_id, output)
+    # except Exception as e:
+    #     logger.error(f"{e},{station_id}-{times}")
     return 0
 
 
@@ -135,13 +149,13 @@ def check_warning(times, station_id, waterlevels):
     limits = [queryset.flood_limit, queryset.warning, queryset.guaranteed]
     types = ['汛限水位', '警戒水位', '保证水位']
     res = [0] * len(limits)
-
     for waterlevel in waterlevels:
         for i in range(len(limits)):
-            if limits[i] and waterlevel > limits[i]:
+            if limits[i] and waterlevel >= limits[i]:
                 res[i] += 1
     if any(res):
         index = 0
+        insert_waring_statistics(times, station_id)
         for i in range(len(res) - 1, -1, -1):
             if res[i] != 0:
                 index = i
@@ -211,15 +225,9 @@ def update(weather_url, water_url):
         else:
             logger.error(f'Unknown type data: {data}')
     sql_time = time.time()
-
-    running_model_list = LSTMModels.objects.filter(is_activate=True).values_list('station_id', flat=True)
-    for station in running_model_list:
-        update_predict(station)
-    lstm_time = time.time()
     logger.debug(
         f'Request CostTime: {net_time - strat_time} '
         f'/ SQL CostTime: {sql_time - net_time} '
-        f'/ LSTM CostTime: {lstm_time - sql_time}'
     )
 
     return 1
