afad14e7aed87eccce0b48cc11e6827542d297f0
mengyonghao <1533512157@qq.com>
2025-05-08 15:47:00 +0800

    告警信息前端接口适配

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("waterinfo/", views.Water_Info.as_view()),
     path("stationCount/", views.StationCount.as_view()),
     path("statistics/", views.StatisticsInfo.as_view()),
+    path("warnings/", views.WarningInfo.as_view()),
     path("test/", views.TaskTest.as_view()),
 ]
